1d980c0791ad731e70897592ff4759ee2a8aeb34
cursedsloth <1337hackern@hotmail.se>
2017-04-12 09:10:02 +0200

    Basic Scene switch with greeting to Employee.

--- src/sample/Controller2.java
+++ src/sample/Controller2.java
@@ -7,34 +7,32 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
-    @FXML private TextField loginUsername;
-    @FXML private TextField loginPassword;
-
+public class Controller2 implements Initializable {
+    @FXML private Label welcomeLabel;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Employee.getInstance();
+        welcomeLabel.setText("Welcome " + Employee.getInstance().getUsername() + "!");
     }
 
     @FXML
-    private void loginEmployee(ActionEvent ae) throws IOException {
-        Employee.getInstance().setUsername(loginUsername.getText());
-
+    private void returnToLogin(ActionEvent ae) throws IOException {
         Node node = (Node)ae.getSource();
         Stage stage = (Stage)node.getScene().getWindow();
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("sceneEmployee.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sceneLogin.fxml"));
         Parent root = loader.load();
 
         Scene scene = new Scene(root, 500, 300);
         stage.setScene(scene);
+
     }
 }
