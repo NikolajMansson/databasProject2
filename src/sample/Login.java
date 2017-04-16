79fcf3548d51f5fd5218f89fff22f821876c65bf
NikolajMansson <nikolaj.mansson@gmail.com>
2017-04-16 18:59:53 +0200

    Add files via upload
    Nya klasser:
    Har lagt in en ReadSequentialFile och en CreateSequentialFile-klass, så att det går att göra Account serializable som det står i klassdiagrammet.
    
    Controller-klass nu "Login":
    Har tagit in klassen Login från klassdiagramet och mergat ihop den med den uppstartade "Controller"-klassen, genom att byta namn på "Controller" till "Login" och hämtat över metoderna från Login-klassen.
    
    Ny klass "SearchAndEdit":
    Har bytt namn på det som i klassdiagramet kallas för Collection, då Java har det som standardklass. Den kallas nu för "SearchAndEdit" och har ett antal subklasser(CustomerCollection, EmployeeCollection, GameCollection och OrderCollection).

--- src/sample/Login.java
+++ src/sample/Login.java
@@ -1,29 +1,57 @@
 package sample;
 
+
+
 /**
- * 
+
+ *
+
  */
+
 public class Login {
 
+
+
     public Login() {
+
     }
+
+
 
     private Account id;
 
+
+
     private Account password;
+
+
 
     private Boolean isGuest;
 
+
+
     public void loginAsCustomer(Account id, Account password, Boolean isGuest) {
+
         // TODO implement here
+
     }
+
+
 
     public void loginAsBoss(Account id, Account password) {
+
         // TODO implement here
+
     }
 
+
+
     public void loginAsEmployee(Account id, Account password) {
+
         // TODO implement here
+
     }
+
+
 
 }
--- src/sample/Order.java
+++ src/sample/Order.java
@@ -1,29 +1,57 @@
 package sample;
 
+
+
 import java.util.Date;
+
+
 
 public class Order extends Cart {
 
+
+
     public Order() {
+
     }
+
+
 
     private Integer orderNumber;
 
+
+
     private CustomerAccount id;
+
+
 
     private Double totalPrice;
 
+
+
     private Integer totalQuantityItems;
+
+
 
     private EmployeeAccount employeeSSN;
 
+
+
     private Date dateOfOrder;
+
+
 
     private Cart cart;
 
 
+
+
+
     public void setCart(Cart cart) {
+
         // TODO implement here
+
     }
+
+
 
 }
