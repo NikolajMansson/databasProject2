98bf779809fcb491bbf358cdddcb1f2289e65acd
cursedsloth <1337hackern@hotmail.se>
2017-04-18 15:46:00 +0200

    Added Login functionality to Boss button, also created a controller for BossWelcomeScene. GUI updated for sceneBoss and sceneLogin.

---
+++ src/sample/BossWelcomeScene.java
@@ -0,0 +1,42 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BossWelcomeScene implements Initializable {
+    @FXML private Label welcomeLabel;
+    ReadActiveUserFile readUser = new ReadActiveUserFile ();
+    BossAccount account = new BossAccount("", "");
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        readUser.openFile ();
+        this.account = (BossAccount) readUser.readRecords ();
+        readUser.closeFile ();
+        welcomeLabel.setText("Welcome " + account.getId() + "!");
+    }
+
+    @FXML
+    private void returnToLogin(ActionEvent ae) throws IOException {
+        Node node = (Node)ae.getSource();
+        Stage stage = (Stage)node.getScene().getWindow();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sceneLogin.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root, 500, 300);
+        stage.setScene(scene);
+
+    }
+}
