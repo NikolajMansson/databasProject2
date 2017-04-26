1b4122a02d5d303d3ff6091cc36c07025b70472d
roadikmaster <masters.sniper_2@yahoo.com>
2017-04-26 13:54:13 +0200

    Singleton class, raw code for total price calculation in cart

--- src/sample/SearchController.java
+++ src/sample/SearchController.java
@@ -1,11 +1,14 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -21,6 +24,37 @@ public class SearchController implements Initializable {
 
     }
 
+    @FXML
+    private RadioButton rb1;
+    @FXML
+    private RadioButton rb2;
+    @FXML
+    private RadioButton rb3;
+    @FXML
+    private RadioButton rb4;
+    @FXML
+    private RadioButton rb5;
+    @FXML
+    private RadioButton rb6;
+    @FXML
+    private TextField searchfield;
+
+    public void searchbyoption()
+    {
+        // if r1 is turned on, then r2 and r3 are turned off
+        // if r2 is turned on, then r1 and r3 are turned off
+        // if r3 is turned on, then r1 and r2 are turned off
+    }
+
+    public void sortbyoption()
+    {
+        // if r4 is turned on, then r5 and r6 are turned off
+        // if r5 is turned on, then r4 and r6 are turned off
+        // if r6 is turned on, then r4 and r5 are turned off
+    }
+
+
+
     public void addToCart()
     {
 
@@ -28,7 +62,14 @@ public class SearchController implements Initializable {
 
     public void search()
     {
+        if (searchfield.getText() == null)
+        {
 
+        }
+        else
+        {
+            
+        }
     }
 
     public void viewCart(ActionEvent ae) throws IOException
